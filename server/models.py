065d0278e7202e550636b2a68d1f0f7819add1d1
Joy Renee Mugo <mugojoy24@gmail.com>
2024-07-10 12:23:35 +0300

    swimmersClub.db was created and model.py was modified.

--- server/models.py
+++ server/models.py
@@ -16,9 +16,12 @@ class Swimmer(db.Model):
 
     coach_id = db.Column(db.Integer, db.ForeignKey('coaches.id'))
     team_id = db.Column(db.Integer, db.ForeignKey('teams.id'))
+    trainingSession_id = db.Column(db.Integer, db.ForeignKey('trainingSessions.id'))
 
     coach = db.relationship("Coach", back_populates="swimmer")
     team = db.relationship('Team', back_populates='swimmers')
+    trainingSession = db.relationship('TrainingSession', back_populates='swimmers')
+    
 
 class Coach(db.Model):
     __tablename__ = 'coaches'
@@ -31,9 +34,11 @@ class Coach(db.Model):
 
     swimmer_id = db.Column(db.Integer, db.ForeignKey('swimmers.id'))
     team_id = db.Column(db.Integer, db.ForeignKey('teams.id'))
+    trainingSession_id = db.Column(db.Integer, db.ForeignKey('trainingSessions.id'))
 
     swimmer = db.relationship("Swimmer", back_populates="coaches")
     team = db.relationship('Team', back_populates='coaches')
+    trainingSession = db.relationship('TrainingSession', back_populates='coaches')
 
 class Team(db.Model):
     __tablename__ = 'teams'
@@ -69,6 +74,7 @@ class TrainingSession(db.Model):
     time = db.Column(db.Time, nullable=True)
     description = db.Column(db.String(100), nullable=True)
     coach_id = db.Column(db.Integer, db.ForeignKey('coaches.id'))
+    student_id = db.Column(db.Integer, db.ForeignKey('swimmers.id'))
 
     team = db.relationship('Team', back_populates='trainingSessions', cascade='all, delete-orphan')
 
